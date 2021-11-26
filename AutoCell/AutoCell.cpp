a815eae362bce9e2be088701b6f4e9cdd8304c4c
Yannick Lonjarret <yannick.lonjarret@gmail.com>
2021-11-26 10:16:42 +0100

    Copie du code
    Copie du code trouvé sur Internet

--- AutoCell/AutoCell.cpp
+++ AutoCell/AutoCell.cpp
@@ -1,20 +1,201 @@
-// AutoCell.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <windows.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+const int gridSize = 25;
+void printGrid(bool gridOne[gridSize+1][gridSize+1]);
+void determineState(bool gridOne[gridSize+1][gridSize+1]);
+void clearScreen(void);
+
+void clearScreen(void) {
+
+    HANDLE hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    DWORD count;
+    DWORD cellCount;
+    COORD homeCoords = { 0, 0 };
+
+    if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+    /* Get the number of cells in the current buffer */
+    GetConsoleScreenBufferInfo( hStdOut, &csbi );
+    cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+    /* Fill the entire buffer with spaces */
+    FillConsoleOutputCharacter(hStdOut, (TCHAR) ' ', cellCount, homeCoords, &count);
+
+    /* Fill the entire buffer with the current colors and attributes */
+    FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, cellCount, homeCoords, &count);
+
+    SetConsoleCursorPosition( hStdOut, homeCoords );
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
 
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+void printGrid(bool gridOne[gridSize+1][gridSize+1]){
+    for(int a = 1; a < gridSize; a++)
+        {
+        for(int b = 1; b < gridSize; b++)
+        {
+            if(gridOne[a][b] == true)
+            {
+                cout << " O ";
+            }
+            else
+            {
+                cout << " . ";
+            }
+            if(b == gridSize-1)
+            {
+                cout << endl;
+            }
+        }
+    }
+}
+
+void compareGrid (bool gridOne[gridSize+1][gridSize+1], bool gridTwo[gridSize+1][gridSize+1]){
+    for(int a =0; a < gridSize; a++)
+    {
+        for(int b = 0; b < gridSize; b++)
+        {
+                gridTwo[a][b] = gridOne[a][b];
+        }
+    }
+}
+
+void determineState(bool gridOne[gridSize+1][gridSize+1]){
+    bool gridTwo[gridSize+1][gridSize+1] = {};
+    compareGrid(gridOne, gridTwo);
+
+    for(int a = 1; a < gridSize; a++)
+    {
+        for(int b = 1; b < gridSize; b++)
+        {
+            int alive = 0;
+            for(int c = -1; c < 2; c++)
+            {
+                for(int d = -1; d < 2; d++)
+                {
+                    if(!(c == 0 && d == 0))
+                    {
+                        if(gridTwo[a+c][b+d])
+                {
+                    ++alive;
+                }
+                    }
+                }
+            }
+            if(alive < 2)
+            {
+                gridOne[a][b] = false;
+            }
+            else if(alive == 3)
+            {
+                gridOne[a][b] = true;
+            }
+            else if(alive > 3)
+            {
+                gridOne[a][b] = false;
+            }
+        }
+    }
+}
+
+int main(){
+
+    // system( "color A" );//LGT green
+
+    clearScreen();
+    bool gridOne[gridSize+1][gridSize+1] = {};
+    int x,y,n;
+    string nc;
+    string start;
+    string filename;
+    cout << "                         THE GAME OF life - Implementation in C++" << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "Also known simply as life, " << endl;
+    cout << "is a cellular automaton devised by the British mathematician John Horton Conway in 1970." << endl;
+    cout << endl;
+    cout << "Rules" << endl;
+    cout << "The universe of the Game of life is an infinite two-dimensional orthogonal grid of square cells," << endl;
+    cout << "each of which is in one of two possible states, life or dead. Every" << endl;
+    cout << "cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent." << endl;
+    cout << "At each step in time, the following transitions occur:" << endl;
+    cout << "1. Any live cell with fewer than two live neighbours dies, as if caused by under-population." << endl;
+    cout << "2. Any live cell with two or three live neighbours lives on to the next generation." << endl;
+    cout << "3. Any live cell with more than three live neighbours dies, as if by over-population." << endl;
+    cout << "4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction." << endl;
+    cout << endl;
+    cout << "O - living cell" << endl;
+    cout << ". - dead cell" << endl;
+    cout << endl;
+    cout << "Enter the number of cells, or 'r' to read cells from file: ";
+    cin >> nc;
+    cout << endl;
+
+    if ( nc == "r" )
+      {
+    while (true)
+      {
+
+        cout << "Enter name of file to read from: "<<endl;
+        cin  >> filename;
+
+        ifstream readfile(filename);
+        if ( readfile.is_open() )
+          {
+        string fileline,xx,yy;
+
+        while (getline(readfile,fileline))
+          {
+            stringstream ss(fileline);
+
+            getline(ss,xx,' ');
+            getline(ss,yy,' ');
+
+            x = stoi(xx);
+            y = stoi(yy);
+
+            gridOne[x][y] = true;
+          }
+        break;
+          } else {
+          cout << "No such file, try again." << endl;
+        }
+      }
+      }
+    else
+      {
+
+    for(int i=0;i<stoi(nc);i++)
+      {
+        cout <<stoi(nc)<< "Enter the coordinates of cell " << i+1 << " : ";
+        cin >> x >> y;
+        gridOne[x][y] = true;
+        printGrid(gridOne);
+      }
+      }
+    cout << "Grid setup is done. Start the game ? (y/n)" << endl;
+    printGrid(gridOne);
+    cin >> start;
+    if( start == "y" || start == "Y" )
+      {
+        for(int i =0; i<2;i++)
+      {
+            printGrid(gridOne);
+            determineState(gridOne);
+            usleep(200000);
+      }
+      }
+    else
+      {
+        return 0;
+      }
+}
